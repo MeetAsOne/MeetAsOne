b1fef11492b59633840f52c0c4d218a1900d9e4d
Seth Deegan <jayandseth@gmail.com>
2024-01-20 23:57:44 +0000

    cleanup header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,21 +1,19 @@
 <script>
-    import { Navbar, NavBrand } from 'flowbite-svelte';
-    import {
-        DarkMode,
-        
-    } from "flowbite-svelte";
+  import { Navbar, NavBrand } from "flowbite-svelte";
+  import { DarkMode } from "flowbite-svelte";
 </script>
 
 <header>
-    <Navbar  >
-        <NavBrand href="/">
-          <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">MeetAsOne</span>
-        </NavBrand>
-        <div>
-            <DarkMode/>
-        </div>
-        
-      </Navbar>
+  <Navbar>
+    <NavBrand href="/">
+      <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+      <span
+        class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+        >MeetAsOne</span
+      >
+    </NavBrand>
+    <div>
+      <DarkMode />
+    </div>
+  </Navbar>
 </header>
-
