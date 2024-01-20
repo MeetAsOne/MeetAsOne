902916201bd260b4316653ab0fa430e50f28acdb
Seth Deegan <jayandseth@gmail.com>
2024-01-20 02:21:21 +0000

    add houdini

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://swyjiuxnroxxokgcyepv.hasura.eu-central-1.nhost.run/v1/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
