f5acf5d3c8f3bc65ef6a6e4b9116801ff3c31b9d
Seth Deegan <jayandseth@gmail.com>
2024-01-20 03:28:55 +0000

    add openai and fix dropdown

--- src/routes/gpt/+page.svelte
+++ src/routes/gpt/+page.svelte
@@ -1,20 +1,23 @@
 <script>
-    import { Button, Dropdown, DropdownItem, Checkbox, Helper } from 'flowbite-svelte';
-    import { ChevronDownSolid } from 'flowbite-svelte-icons';
-  </script>
-  
-  <Button>Dropdown checkbox<ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" /></Button>
-  <Dropdown class="w-60 p-3 space-y-1 text-sm">
-    <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-      <Checkbox>Enable notifications</Checkbox>
-      <Helper class="ps-6">Some helpful instruction goes over here.</Helper>
-    </li>
-    <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-      <Checkbox checked>Enable 2FA auth</Checkbox>
-      <Helper class="ps-6">Some helpful instruction goes over here.</Helper>
-    </li>
-    <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-      <Checkbox>Subscribe newsletter</Checkbox>
-      <Helper class="ps-6">Some helpful instruction goes over here.</Helper>
-    </li>
-  </Dropdown>
+    import {
+        Button,
+        Dropdown,
+        DropdownItem,
+        Checkbox,
+        Helper,
+    } from "flowbite-svelte";
+    import { ChevronDownSolid } from "flowbite-svelte-icons";
+</script>
+
+<div>
+    <Button
+        >Import calendar<ChevronDownSolid
+            class="w-3 h-3 ms-2 text-white dark:text-white"
+        /></Button
+    >
+    <Dropdown class="w-60 p-3 space-y-1 text-sm">
+        <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+            <Helper class="ps-6">Scan calendar photo</Helper>
+        </li>
+    </Dropdown>
+</div>
