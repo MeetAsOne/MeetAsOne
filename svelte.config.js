e0e09a502c27c0502a4b87a713ac3c2386389c1e
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-01-19 22:10:37 -0500

    Install deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$houdini: './$houdini',
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $houdini: "./$houdini",
+    },
+  },
+
+  preprocess: [vitePreprocess({})],
 };
 
 export default config;
