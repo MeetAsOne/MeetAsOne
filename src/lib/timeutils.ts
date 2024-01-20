81326d73ad9321763ca7e3d0f102444798a167d2
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-01-20 15:27:22 -0500

    fix typo

--- src/lib/timeutils.ts
+++ src/lib/timeutils.ts
@@ -6,10 +6,10 @@ export function timeToInt(timeString: string) {
   return (Number(hours) + Number(ampm?.toLowerCase() === "pm") * 12) * 60 + Number(minutes);
 }
 
-export function intToTime(midnightOffset: number, millitary = false) {
+export function intToTime(midnightOffset: number, military = false) {
   const hours = Math.floor(midnightOffset / 60);
   const minutes = midnightOffset % 60;
-  return `${millitary ? hours : (hours <= 12 ? hours : hours % 13 + 1)}:${String(minutes).padStart(2, "0")}` + (millitary ? "" : (hours > 12 ? " PM" : " AM"));
+  return `${military ? hours : (hours <= 12 ? hours : hours % 13 + 1)}:${String(minutes).padStart(2, "0")}` + (military ? "" : (hours > 12 ? " PM" : " AM"));
 }
 
 export const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] as const;
