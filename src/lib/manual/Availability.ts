6307060f6b81c8e42eb7579a00b11e1cfd3fa3f2
Seth Deegan <jayandseth@gmail.com>
2024-01-20 19:22:33 +0000

    fix types

--- src/lib/manual/Availability.ts
+++ src/lib/manual/Availability.ts
@@ -1,10 +1,11 @@
 /** Key: the date in any parsable format. Value: array of indices to time blocks.
  * For example, if you ask for times starting at 8am and use 15 min intervals, 9:30 will be index 5  */
-export type Availability = Record<string, number[]>
+type DateStr = string;
+export type Availability = Record<DateStr, number[]>
 
 /** Converts availability from format in database or localstorage into format component can read */
-export function loadAvailability(...availabilities: any[]) {
-  let unpackedAvailability = {};
+export function loadAvailability(...availabilities: Availability[]) {
+  const unpackedAvailability: Availability = {};
   for (const availability of availabilities) {
     for (const key in availability) {
       unpackedAvailability[key] = [];
@@ -13,22 +14,22 @@ export function loadAvailability(...availabilities: any[]) {
       }
     }
   }
-  return unpackedAvailability as any;
+  return unpackedAvailability;
 }
 
 /** Converts from component representation to database representation */
-export function compactAvailability(availability: any) {
-  const formattedAvailability = {};
-  const weeklyAvailability = {};
+export function compactAvailability(availability: Availability) {
+  const formattedAvailability: Availability = {};
+  const weeklyAvailability: Availability = {};
   for (const key in availability) {
-    formattedAvailability[key] = availability[key].map((numAvailble, idx) => numAvailble ? idx : null).filter(a => a != null);
+    formattedAvailability[key] = availability[key].map((numAvailble, idx) => numAvailble ? idx : null).filter((a): a is number => a != null);
     weeklyAvailability[new Date(key).getDay()] = formattedAvailability[key];
   }
-  return [formattedAvailability as any, weeklyAvailability] as const;
+  return [formattedAvailability, weeklyAvailability] as const;
 }
 
 export function applyAvailability(dates: number[], availability: any) {
-  const out = {};
+  const out: Availability = {};
   for (const date of dates) {
     out[new Date(date).toLocaleDateString()] = availability[new Date(date).getDay()] ?? [];
   }
