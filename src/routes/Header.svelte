7c98d0765750f8ddbe2734a22047287db37b5208
lectrician1 <jayandseth@gmail.com>
2024-01-21 02:59:22 -0500

    adjust header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,29 +2,27 @@
   import { Navbar, NavBrand } from "flowbite-svelte";
   import { DarkMode } from "flowbite-svelte";
   import logo from "$lib/images/logo.svg";
-//   import { Video } from 'flowbite-svelte';
+  import githubLogo from "$lib/images/github.svg"
+  //   import { Video } from 'flowbite-svelte';
 </script>
 
 <header>
   <Navbar>
     <NavBrand href="/">
-		<span class="welcome">
-			<img
-				src={logo}
-				alt="MeetAsOne"
-				style="width: 40px; height: auto; margin-right:16px"
-			
-			/>
-		</span>
+      <span class="welcome">
+        <img
+          src={logo}
+          alt="MeetAsOne"
+          style="width: 40px; height: auto; margin-right:16px"
+        />
+      </span>
       <!-- <Video src="/images/logo.svg" controls trackSrc="logo.svg" /> -->
-	  <!-- <span
+      <!-- <span
 	    
         class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
         >MeetAsOne</span
       > -->
     </NavBrand>
-    <div>
-      <DarkMode />
-    </div>
+    <DarkMode />
   </Navbar>
 </header>
