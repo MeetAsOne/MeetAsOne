78e90b0aa0df475d240d97e606d49eee7dc4777e
singh895 <singh895@purdue.edu>
2024-01-21 00:10:51 -0500

    logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "../app.pcss";
     import Header from './Header.svelte';
+	import Footer from "./Footer.svelte";
     import './styles.css';
 </script>
 
@@ -10,6 +11,7 @@
 	<main>
 		<slot></slot>
 	</main>
+	<Footer></Footer>
 </div>
 
 <style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,27 @@
 <script>
   import { Navbar, NavBrand } from "flowbite-svelte";
   import { DarkMode } from "flowbite-svelte";
+  import logo from "$lib/images/logo.svg";
+//   import { Video } from 'flowbite-svelte';
 </script>
 
 <header>
   <Navbar>
     <NavBrand href="/">
-      <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
-      <span
+		<span class="welcome">
+			<img
+				src={logo}
+				alt="MeetAsOne"
+				style="width: 40px; height: auto; margin-right:16px"
+			
+			/>
+		</span>
+      <!-- <Video src="/images/logo.svg" controls trackSrc="logo.svg" /> -->
+	  <!-- <span
+	    
         class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
         >MeetAsOne</span
-      >
+      > -->
     </NavBrand>
     <div>
       <DarkMode />
