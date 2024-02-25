99d569f6b9022b213834033e5c7e9101cd0a1952
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-02-24 19:31:52 -0500

    Document range fn

--- src/lib/range.ts
+++ src/lib/range.ts
@@ -1,4 +1,5 @@
-// pretty nieve implementation but idk
+/** Makes a list of numbers from start (inclusive) to stop (exclusive) in `step` increments */
 export default function range(start: number, stop: number, step = 1) {
+  // pretty nieve implementation but idk
   return [...Array(Math.ceil((stop - start) / step)).keys()].map(i => i * step + start);
 }
