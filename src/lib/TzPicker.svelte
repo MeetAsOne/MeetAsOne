5b04d65510b0723ca73270a81cea2e42c2d0a58b
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-03-23 22:34:35 -0400

    Expand timezone offerings

--- src/lib/TzPicker.svelte
+++ src/lib/TzPicker.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-  import {timezones} from "$lib/timeutils";
+  import {getAllTzNames, getLocalTzName, getTzOffset} from "$lib/timeutils";
   import {ChevronDownSolid} from "flowbite-svelte-icons";
   import {Button, Dropdown, DropdownItem, Search} from "flowbite-svelte";
 
+  /** Name of timezone */
+  let selectedTimezoneName = getLocalTzName();
+
   /** Minutes difference from UTC */
-  export let selectedTimezone = new Date().getTimezoneOffset();
+  export let selectedTimezone: number;
+  $: selectedTimezone = getTzOffset(selectedTimezoneName);
 
   let showDropdown = false;
   let searchQuery = "";
 
-  function handleOptionSelect(option: number) {
-    selectedTimezone = option;
+  // TODO: it would be nice if selected timezone scrolled into view, but ehhhhh
+
+  function handleOptionSelect(option: string) {
+    selectedTimezoneName = option;
     showDropdown = false;
   }
 </script>
 
 <Button>
-    {globalThis.window ? timezones.get(selectedTimezone) : "Select timezone"}
+    {globalThis.window ? selectedTimezoneName : "Select timezone"}
     <ChevronDownSolid class="w-3 h-3 ms-2 text-white dark:text-white" />
 </Button>
 <Dropdown
@@ -26,12 +32,12 @@
     <div slot="header" class="p-3">
         <Search size="md" bind:value={searchQuery} autofocus />
     </div>
-    {#each timezones.entries() as [tzOffset, tzName]}
+    {#each getAllTzNames() as tzName}
         {#if tzName.toLowerCase().includes(searchQuery.toLowerCase())}
             <li
                     class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600"
             >
-                <DropdownItem on:click={() => handleOptionSelect(tzOffset)}>
+                <DropdownItem on:click={() => handleOptionSelect(tzName)}>
                     {tzName}
                 </DropdownItem>
             </li>
--- src/lib/timeutils.ts
+++ src/lib/timeutils.ts
@@ -27,37 +27,6 @@ export function intToTime(midnightOffset: number, military = false) {
 /** Milliseconds since epoch for date string */
 export const dateStrToEpoch = (dateStr: string) => new Date(dateStr).getTime();
 
-/** Maps `Date.getTimezoneOffset` values to timezone names. NOTE: keys are negated from label & in minutes */
-export const timezones = new Map([
-  [HOUR * 12, "(GMT-12:00) International Date Line West"],
-  [HOUR * 11, "(GMT-11:00) Midway Island, Samoa"],
-  [HOUR * 10, "(GMT-10:00) Hawaii"],
-  [HOUR * 9, "(GMT-09:00) Alaska"],
-  [HOUR * 8, "(GMT-08:00) Pacific Time (US & Canada)"],
-  [HOUR * 7, "(GMT-07:00) Mountain Time (US & Canada)"],
-  [HOUR * 6, "(GMT-06:00) Central Time (US & Canada)"],
-  [HOUR * 5, "(GMT-05:00) Eastern Time (US & Canada)"],
-  [HOUR * 4, "(GMT-04:00) Atlantic Time (Canada)"],
-  [HOUR * 3, "(GMT-03:00) Buenos Aires, Georgetown"],
-  [HOUR * 2, "(GMT-02:00) Mid-Atlantic"],
-  [HOUR, "(GMT-01:00) Cape Verde Islands"],
-  [0, "(GMT) Greenwich Mean Time, London"],
-  [HOUR * -1, "(GMT+01:00) Brussels, Copenhagen, Madrid, Paris"],
-  [HOUR * -2, "(GMT+02:00) Athens, Istanbul, Cairo"],
-  [HOUR * -3, "(GMT+03:00) Moscow, St. Petersburg, Volgograd"],
-  [HOUR * -4, "(GMT+04:00) Dubai, Abu Dhabi, Muscat"],
-  [HOUR * -5, "(GMT+05:00) Karachi, Islamabad, Tashkent"],
-  [HOUR * -5.5, "(GMT+05:30) Indian Standard Time"],
-  [HOUR * -6, "(GMT+06:00) Almaty, Dhaka"],
-  [HOUR * -7, "(GMT+07:00) Bangkok, Hanoi, Jakarta"],
-  [HOUR * -8, "(GMT+08:00) Beijing, Hong Kong, Singapore"],
-  [HOUR * -9, "(GMT+09:00) Tokyo, Seoul, Osaka"],
-  [HOUR * -9.5, "(GMT+09:30) Adelaide, Darwin"],
-  [HOUR * -10, "(GMT+10:00) Brisbane, Sydney, Melbourne"],
-  [HOUR * -11, "(GMT+11:00) Solomon Islands, New Caledonia"],
-  [HOUR * -12, "(GMT+12:00) Fiji, Kamchatka, Marshall Is."],
-]);
-
 /** Converts a date to the format yyyy-mm-dd, regardless of locale or timezone. Not displayed, used internally */
 export function canonicalDateStr(date: Date) {
   return date.toISOString().substring(0, 10) as DateStr;  // Remove the time component
@@ -127,3 +96,17 @@ export function offsetDate(date: Date | string | number, offsetMin: number) {
 export function offsetRange(ranges: DatetimeRange[], offsetMin: number) {
   return ranges.map(range => range.map(timestamp => offsetDate(timestamp, offsetMin).getTime() * MILLISECOND) as DatetimeRange);
 }
+
+/**
+ * @return A timezone offset in minutes given a timezone name
+ * @see https://stackoverflow.com/a/68593283
+ */
+export function getTzOffset(timeZone = 'UTC', date = new Date()) {
+  const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
+  const tzDate = new Date(date.toLocaleString('en-US', { timeZone }));
+  return (tzDate.getTime() - utcDate.getTime()) * MILLISECOND;
+}
+
+export const getLocalTzName = () => Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+export const getAllTzNames = () => Intl.supportedValuesOf('timeZone');
